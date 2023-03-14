725f4686f71a16a981c2567ef13e04690d3f27eb
Enoisong <enobongitiaba988@gmail.com>
2023-03-14 23:20:06 +0100

    Update script.js file

--- Script.js
+++ Script.js
@@ -28,9 +28,9 @@ const projects = [
     work: 'Back End Dev',
     date: 2015,
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    techUse: ['html', 'css', 'javaScript'],
+    techUse: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstrap'],
     projectDemo: 'https://enoisong.github.io/Mobile-First-Portfolio/',
-    projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio.git',
+    projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio',
   },
 
   {
@@ -43,7 +43,7 @@ const projects = [
     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming <br> their friends.',
     techUse: ['html', 'Ruby on rails', 'css', 'javaScript'],
     projectDemo: 'https://enoisong.github.io/Mobile-First-Portfolio/',
-    projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio.git',
+    projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio',
   },
 
   {
@@ -56,7 +56,7 @@ const projects = [
     description: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     techUse: ['html', 'Ruby on rails', 'css', 'javaScript'],
     projectDemo: 'https://enoisong.github.io/Mobile-First-Portfolio/',
-    projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio.git',
+    projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio',
   },
 
   {
@@ -69,7 +69,7 @@ const projects = [
     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     techUse: ['html', 'Ruby on rails', 'css', 'javaScript'],
     projectDemo: 'https://enoisong.github.io/Mobile-First-Portfolio/',
-    projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio.git',
+    projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio',
   },
 ];
 const mainContainer = document.getElementById('workscontainer');
@@ -80,54 +80,88 @@ function popupFunc(id) {
   let oneCard = projects.filter((el) => el.id === id);
   oneCard = oneCard.shift();
   const popupContent = ` <div class="container windowPopup popup">
-                <div class="pop-header">
-                    <h4 class="h-4">${oneCard.name}</h4>
-                    <span class="closePopup" onclick="closePopupFunc()"><i class="fa fa-times"></i></span>
-                    <ul class="pop-description-element">
-                        <li class="element" id="companyID">${oneCard.companyId}</li>
-                        <li class="element">
-                            <img class="bullet" src="./images/bullet.png" alt="bullet" />
-                        </li>
-                        <li class="element" id="workType">${oneCard.work}</li>
-                        <li class="element">
-                            <img class="bullet" src="./images/bullet.png" alt="bullet" />
-                        </li>
-                        <li class="element" id="date">${oneCard.date}</li>
-                    </ul>
-                </div>
-                <img class="work-pic" src=${oneCard.pic} alt="landing page view" />
-                <img class="work-pic pop-img" src=${oneCard.pic} alt="nature image" />
-                <div class="pop-element">
-                    <p class="description-text">
-                        ${oneCard.description}
-                    </p>
-                    <div class="pop-items">
-                        <ul class="languages">
-                          ${oneCard.techUse.map((el) => `<li class="lang">${el}</li>`).join('')}
-                        </ul>
-                        <div class="bottom-bar"></div>
-                        <div class="navigation-links">
-                           
-                        </div>
-                    </div>
-                </div>
-            </div>`;
-  windowPopup.style.display = 'flex';
-  navigationBarClose.style.display = 'none';
-  document.body.style.overflow = 'hidden';
-  popup.innerHTML = popupContent;
+  <div class="pop-header">
+      <h4 class="h-4">${oneCard.name}</h4>
+      <span class="closePopup" onclick="closePopupFunc()"><i class="fa fa-times"></i></span>
+      <ul class="pop-description-element">
+          <li class="element" id="companyID">${oneCard.companyId}</li>
+          <li class="element">
+              <img class="bullet" src="./images/bullet.png" alt="bullet" />
+          </li>
+          <li class="element" id="workType">${oneCard.work}</li>
+          <li class="element">
+              <img class="bullet" src="./images/bullet.png" alt="bullet" />
+          </li>
+          <li class="element" id="date">${oneCard.date}</li>
+      </ul>
+  </div>
+  <img class="work-pic" src=${oneCard.pic} alt="landing page view" />
+  <img class="work-pic pop-img" src=${oneCard.pic} alt="nature image" />
+  <div class="pop-element">
+      <p class="description-text">
+          ${oneCard.description}
+      </p>
+      <div class="pop-items">
+          <ul class="languages">
+            ${oneCard.techUse.map((el) => `<li class="lang">${el}</li>`).join('')}
+          </ul>
+          <div class="bottom-bar"></div>
+          <div class="navigation-links">
+              <form action=${oneCard.projectDemo} class="btnSource">
+                  <span class="popup-link-icon">
+                    <input  type="submit" value="See live" />
+                    <img src="./images/Icon-Export.png" alt="See live">
+                  </span>
+              </form>
+              <form action=${oneCard.projectSource} class="btnSource">
+                  <span class="popup-link-icon">
+                    <input  type="submit" value="See Source" />
+                    <img src="./images/Icon-GitHub.png" alt="See source">
+                  </span>
+              </form>
+          </div>
+      </div>
+  </div>
+</div>`;
+windowPopup.style.display = 'flex';
+navigationBarClose.style.display = 'none';
+document.body.style.overflow = 'hidden';
+popup.innerHTML = popupContent;
 }
 function closePopupFunc(status) {
-  if (status === null) return;
-  windowPopup.style.display = 'none';
-  navigationBarClose.style.display = 'flex';
-  document.body.style.overflow = 'scroll';
+if (status === null) return;
+windowPopup.style.display = 'none';
+navigationBarClose.style.display = 'flex';
+document.body.style.overflow = 'scroll';
 }
 function worksContainer() {
-  const element = projects.map((val, index) => {
-    const cards = index % 2 === 0 ? `<div class="container">
-  <img class="work-pic" src="./images/Portoflio_Card/work.png" alt="nature image" />
-  <img class="work-pic" src=${val.pic} alt="landing page view" />
+const element = projects.map((val, index) => {
+const cards = index % 2 === 0 ? `<div class="container">
+<img class="work-pic" src="./images/Portoflio_Card/work.png" alt="nature image" />
+<img class="work-pic" src=${val.pic} alt="landing page view" />
+<div class="work-description">
+<h4>${val.name}</h4>
+<ul class="description-element">
+<li>${val.companyId}</li>
+<li class="element">
+<img class="bullet" src="./images/bullet.png" alt="bullet" />
+</li>
+<li class="element">${val.work}</li>
+<li class="element">
+<img class="bullet" src="./images/bullet.png" alt="bullet" />
+</li>
+<li class="element">${val.date}</li>
+</ul>
+<p class="description-text">${val.description}</p>
+<ul class="languages">
+${val.techUse.map((el) => `<li class="lang">${el}</li>`).join('')}
+</ul><br>
+
+<button class="btn openPopup" id="firstBtn" type="submit" onclick="popupFunc(${val.id})">See Project</button>
+</div>
+</div>`
+: ` <div class="container">
+  <img class="work-pic" src="./images/Portoflio_Card/work1.png" alt="multi-post stories image" />
   <div class="work-description">
       <h4>${val.name}</h4>
       <ul class="description-element">
@@ -143,44 +177,21 @@ function worksContainer() {
       </ul>
       <p class="description-text">${val.description}</p>
       <ul class="languages">
-       ${val.techUse.map((el) => `<li class="lang">${el}</li>`).join('')}
+          ${val.techUse.map((el) => `<li class="lang">${el}</li>`).join('')}
       </ul><br>
-
-      <button class="btn openPopup" id="firstBtn" type="submit" onclick="popupFunc(${val.id})">See Project</button>
+      <button class="btn openPopup" id="secondBtn" onclick="popupFunc(${val.id})" type="submit">See
+          Project</button>
   </div>
-</div>`
-      : `            <div class="container">
-                <img class="work-pic" src="./images/Portoflio_Card/work1.png" alt="multi-post stories image" />
-                <div class="work-description">
-                    <h4>${val.name}</h4>
-                    <ul class="description-element">
-                        <li>${val.companyId}</li>
-                        <li class="element">
-                            <img class="bullet" src="./images/bullet.png" alt="bullet" />
-                        </li>
-                        <li class="element">${val.work}</li>
-                        <li class="element">
-                            <img class="bullet" src="./images/bullet.png" alt="bullet" />
-                        </li>
-                        <li class="element">${val.date}</li>
-                    </ul>
-                    <p class="description-text">${val.description}</p>
-                    <ul class="languages">
-                        ${val.techUse.map((el) => `<li class="lang">${el}</li>`).join('')}
-                    </ul><br>
-                    <button class="btn openPopup" id="secondBtn" onclick="popupFunc(${val.id})" type="submit">See
-                        Project</button>
-                </div>
-                <img class="work-pic-1" src=${val.pic} alt="professional art printing image" />
-            </div>`;
-    return cards;
-  });
-  return element;
+  <img class="work-pic-1" src=${val.pic} alt="professional art printing image" />
+</div>`;
+return cards;
+});
+return element;
 }
 
 window.addEventListener('load', () => {
-  mainContainer.innerHTML = worksContainer();
-  return mainContainer;
+mainContainer.innerHTML = worksContainer();
+return mainContainer;
 });
 popupFunc(null);
 closePopupFunc(null);
