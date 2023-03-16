35036ccccccd0f84682fccb2967738366bad2e26
Enoisong <enobongitiaba988@gmail.com>
2023-03-17 00:18:45 +0100

    Fix linters

--- form.js
+++ form.js
@@ -1,26 +1,21 @@
 function showMessage(message) {
     const msg = document.querySelector('small');
     msg.innerText = message;
-  }
-  
+}  
   function showError(message) {
     showMessage(message);
-  }
-  
+}  
   function showSuccess() {
     showMessage('');
-  }
-  
+}  
   function validateEmail(input, invalidMsg) {
-    const emailRegex = /[A-Z]/;
-  
+    const emailRegex = /[A-Z]/;  
     const email = input.value.trim();
     if (emailRegex.test(email)) {
       return showError(invalidMsg);
-    }
+}
     return true;
-  }
-  
+}  
   const EMAIL_INVALID = 'Please enter your email address in lower case';
   const form = document.querySelector('form');
   form.addEventListener('submit', (event) => {
@@ -30,6 +25,6 @@ function showMessage(message) {
     if (emailValid) {
       showSuccess();
       form.submit();
-    }
+}
   });
   
