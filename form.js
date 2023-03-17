01018df4fb7e1236f9a80d376b95916584288022
Enoisong <enobongitiaba988@gmail.com>
2023-03-18 00:16:21 +0100

    Edit script.js File

--- form.js
+++ form.js
@@ -1,29 +1,47 @@
-function showMessage(message) {
-  const msg = document.querySelector('small');
-  msg.innerText = message;
-}
-function showError(message) {
-  showMessage(message);
-}
-function showSuccess() {
-  showMessage('');
-}
-function validateEmail(input, invalidMsg) {
-  const emailRegex = /[A-Z]/;
-  const email = input.value.trim();
-  if (emailRegex.test(email)) {
-    return showError(invalidMsg);
-  }
-  return true;
-}
-const EMAIL_INVALID = 'Please enter your email address in lower case';
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const email = document.querySelector("input[type='email']");
-  const emailValid = validateEmail(email, EMAIL_INVALID);
-  if (emailValid) {
-    showSuccess();
-    form.submit();
-  }
+const toggle = document.querySelector('.toggle');
+const body = document.querySelector('body');
+const menuOptions = document.querySelectorAll('.menu li');
+
+toggle.addEventListener('click', () => {
+  body.classList.toggle('open');
 });
+
+menuOptions.forEach((option) => {
+  option.addEventListener('click', () => {
+    body.classList.remove('open');
+  });
+});
+//    ----------------------------Storage----------------------------------------------
+
+const nameInput = document.querySelector('.inp-name');
+const emailInput = document.querySelector('.inp-email');
+const messageInput = document.querySelector('.inp-text');
+
+// Define
+function saveFormData() {
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+// function to set
+function loadFormData() {
+  const savedFormData = localStorage.getItem('formData');
+  if (savedFormData) {
+    const formData = JSON.parse(savedFormData);
+    nameInput.value = formData.name;
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+}
+
+// Add event listeners to the input fields
+nameInput.addEventListener('input', saveFormData);
+emailInput.addEventListener('input', saveFormData);
+messageInput.addEventListener('input', saveFormData);
+
+// Calling
+loadFormData();
