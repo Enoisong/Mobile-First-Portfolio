7ea1b651f0509f8c41afe30da90460a0600d9c69
Enoisong <enobongitiaba988@gmail.com>
2023-03-18 03:20:32 +0100

    Edit  script.js File

--- form.js
+++ form.js
@@ -32,7 +32,7 @@ form.addEventListener('submit', (event) => {
   }
 });
 const form = document.querySelector('form');
-  form.addEventListener('submit', (event) => {
+form.addEventListener('submit', (event) => {
   // stop form submission
   event.preventDefault(); 
 });
