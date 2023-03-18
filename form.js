25188a5c9193d4dd32301fe841f4a361e50141d9
Enoisong <enobongitiaba988@gmail.com>
2023-03-18 03:24:11 +0100

    Fix linters

--- form.js
+++ form.js
@@ -31,7 +31,6 @@ form.addEventListener('submit', (event) => {
     form.submit();
   }
 });
-const form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
   // stop form submission
   event.preventDefault(); 
