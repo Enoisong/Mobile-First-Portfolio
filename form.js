548e9a760ea5ac54cbc490f229f4defaeb2e9b0d
Enoisong <enobongitiaba988@gmail.com>
2023-03-18 02:25:45 +0100

    Update script.js File

--- form.js
+++ form.js
@@ -1,47 +1,7 @@
-const toggle = document.querySelector('.toggle');
-const body = document.querySelector('body');
-const menuOptions = document.querySelectorAll('.menu li');
-
-toggle.addEventListener('click', () => {
-  body.classList.toggle('open');
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {});
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  // stop form submission
+  event.preventDefault();
 });
-
-menuOptions.forEach((option) => {
-  option.addEventListener('click', () => {
-    body.classList.remove('open');
-  });
-});
-//    ----------------------------Storage----------------------------------------------
-
-const nameInput = document.querySelector('.inp-name');
-const emailInput = document.querySelector('.inp-email');
-const messageInput = document.querySelector('.inp-text');
-
-// Define
-function saveFormData() {
-  const formData = {
-    name: nameInput.value,
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  localStorage.setItem('formData', JSON.stringify(formData));
-}
-
-// function to set
-function loadFormData() {
-  const savedFormData = localStorage.getItem('formData');
-  if (savedFormData) {
-    const formData = JSON.parse(savedFormData);
-    nameInput.value = formData.name;
-    emailInput.value = formData.email;
-    messageInput.value = formData.message;
-  }
-}
-
-// Add event listeners to the input fields
-nameInput.addEventListener('input', saveFormData);
-emailInput.addEventListener('input', saveFormData);
-messageInput.addEventListener('input', saveFormData);
-
-// Calling
-loadFormData();
